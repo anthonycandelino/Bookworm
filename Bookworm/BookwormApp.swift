6c1e640e44a920d9a474df97655be4ab9159ac9f
Anthony Candelino <anthcandelino@gmail.com>
2024-08-26 21:17:33 -0400

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Anthony Candelino on 2024-08-26.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
